2360c9a9327019c3a02e34a79b22738d2991804c
Chidinma <nwoyechidinma518@gmail.com>
2022-09-19 18:16:52 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+// imports all exported functions from the file
+const notes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',  //not necessary just for understanding
+    // builder contains the argv
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    // argv are the attributes of the builder object
+    handler: function (argv) {
+// notes imports two functions. call the one you need i.e addnote
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+// to parse strings. Without this yargs.commands will be inactive
+yargs.parse()
---
+++ notes.js
@@ -0,0 +1,92 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// savenotes overwrites file and adds new notes
+const saveNotes = function (notes) {
+    // json.stringify converts objects to string
+        const dataJSON = JSON.stringify(notes)
+        fs.writeFileSync('notes.json', dataJSON)
+    }
+    
+//This function loads already available notes. 
+// If any line of code under try fails, the code stops running and runs catch instead
+const loadNotes = function () {
+    try {
+        // readfilesync reads files in bits. use .tostring to convert bits to string
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        // use json.parse to convert string to object. The loadNotes funtion returns the converted object 
+        // from the notes.json file
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+// create a function to add notes. Function takes in two arguements
+const addNote = function (title, body) {
+    const notes = loadNotes()
+// create a function to avoid duplicate names with same title. 
+// if a note within notes has same title with addnote title, add it to duplicateNotes array. .filter forms array
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+// if array of duplicate notes is empty, push new note
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+// remove note based on title
+const removeNote = function (title) {
+    const notes = loadNotes()
+//if a note within notes has a title same as the title to be removed, it wil not be included in notestokeep
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
+}
+
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.inverse('Your notes'))
+
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not found!'))
+    }
+}
+
+// exports getnotes and addnote functions. These functions can then be used in other files
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
